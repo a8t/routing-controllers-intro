53aac4eeaf7ce6859ee82be394bb79141ac73a5d
Andy Tran <andy8tran@gmail.com>
2017-08-30 19:12:26 -0400

    kitten urls added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,25 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "welcome"
+    render :welcome
+  end
+
+  def about
+    @header = "About!"
     render :about
   end
 
+  def contest
+    @header = "Contest!"
+    render :contest
+  end
+
+  def kitten
+    @header = "Contest!"
+    size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{size}/#{size}/cats"
+    render :kitten
+  end
 
 end
